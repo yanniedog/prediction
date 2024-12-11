89726af7be9bff5ae7188aea7acb5e10fce08829
yanniedog <jkokavec@gmail.com>
2024-12-11 10:33:43 +0000

    Dec 11, 2024, 9:33 PM

--- scripts/linear_regression.py
+++ scripts/linear_regression.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 import os
-from datetime import timedelta
+from datetime import datetime, timedelta
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.utils import resample
