a6c9a16f95025e0794cf7a6176bc520e98ac4cf5
yanniedog <jkokavec@gmail.com>
2024-12-11 14:02:54 +0000

    Dec 12, 2024, 1:02 AM

--- launch.py
+++ launch.py
@@ -1,4 +1,4 @@
-# filename: launch.py
+# launch.py
 import os
 import sys
 from pathlib import Path
@@ -6,7 +6,27 @@ from datetime import datetime
 import logging
 import runpy
 
-# Remove existing log files
+if 'VIRTUAL_ENV' not in os.environ:
+
+    venv_path = Path.cwd() / 'venv'
+    if not venv_path.is_dir():
+        print("Virtual environment not found. Please create it using 'python -m venv venv'.")
+        sys.exit(1)
+
+    if sys.platform == 'win32':
+        activate_script = venv_path / 'Scripts' / 'activate.bat'
+    else:
+        activate_script = venv_path / 'bin' / 'activate'
+
+    if sys.platform == 'win32':
+        os.system(f'call {activate_script}')
+    else:
+        os.system(f'source {activate_script}')
+
+    print("Virtual environment activated.")
+
+sys.path.append(str(Path.cwd() / 'scripts'))
+
 for f in Path.cwd().glob('*.log'):
     f.unlink()
 
@@ -22,6 +42,9 @@ file_handler = logging.FileHandler(log_filename, mode='w')
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 logger.addHandler(file_handler)
+
+font_manager_logger = logging.getLogger('matplotlib.font_manager')
+font_manager_logger.setLevel(logging.WARNING)
 
 class DoubleWriter:
     def __init__(self, stdout, stderr, logger):
@@ -41,8 +64,11 @@ sys.stdout = DoubleWriter(sys.__stdout__, sys.__stderr__, logger)
 sys.stderr = DoubleWriter(sys.__stderr__, sys.__stderr__, logger)
 
 try:
-    runpy.run_path("start.py", run_name="__main__")
+
+    start_path = str(Path.cwd() / 'scripts' / 'start.py')
+    runpy.run_path(start_path, run_name='__main__')
 except SystemExit as e:
     sys.exit(e.code)
-except:
-    raise
+except Exception as e:
+    logger.exception("An error occurred while running the script.")
+    sys.exit(1)
