15df9c40c36167570fc26b2161e4bccdbdd132f0
yanniedog <jkokavec@gmail.com>
2024-12-13 08:29:21 +0000

    Dec 13, 2024, 7:29 PM

--- launch.py
+++ launch.py
@@ -3,10 +3,7 @@ import sys
 import logging
 import runpy
 from pathlib import Path
-from datetime import datetime
 from logging_setup import configure_logging
-from sqlite_data_manager import initialize_database
-from config import DB_PATH
 from backup_utils import run_backup_cleanup
 
 def delete_old_logs(log_dir: Path, log_extension: str = ".log"):
@@ -19,19 +16,12 @@ def delete_old_logs(log_dir: Path, log_extension: str = ".log"):
 def main():
     current_dir = Path.cwd()
 
-    # Ensure old logs are cleaned up
     delete_old_logs(current_dir)
-
-    # Pass only the log prefix to configure_logging
     configure_logging(log_file_prefix=current_dir.name)
     logger = logging.getLogger()
 
     try:
         run_backup_cleanup()
-        initialize_database(DB_PATH)
-        logger.info("Database initialized.")
-
-        # Run the main start script
         runpy.run_path("start.py", run_name="__main__")
     except SystemExit as e:
         sys.exit(e.code)
