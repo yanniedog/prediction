389091dba597f7eb66c249fd287f5684f02e6c05
yanniedog <jkokavec@gmail.com>
2024-12-17 02:40:42 +0000

    Dec 17, 2024, 1:40 PM

--- indicator_config_parser.py
+++ indicator_config_parser.py
@@ -1,17 +1,13 @@
 # indicator_config_parser.py
 import json
-from typing import Dict, List, Optional
 
-def parse_indicators_json(indicator_params_path: str = 'indicator_params.json') -> Dict[str, Dict]:
-    with open(indicator_params_path, 'r') as f:
-        indicator_params = json.load(f)
-    return indicator_params
+def parse_indicators_json(indicator_params_path='indicator_params.json'):
+    with open(indicator_params_path,'r')as f:return json.load(f)
 
-def get_configurable_indicators(indicator_params_path: str = 'indicator_params.json') -> List[str]:
-    indicator_params = parse_indicators_json(indicator_params_path)
-    return sorted(indicator_params.get("indicators", {}).keys())
+def get_configurable_indicators(indicator_params_path='indicator_params.json'):
+    i=parse_indicators_json(indicator_params_path)
+    return sorted(i.get("indicators",{}).keys())
 
-
-def get_indicator_parameters(indicator_name: str, indicator_params_path: str = 'indicator_params.json') -> Optional[Dict]:
-    indicator_params = parse_indicators_json(indicator_params_path)
-    return indicator_params.get(indicator_name)
+def get_indicator_parameters(indicator_name,indicator_params_path='indicator_params.json'):
+    i=parse_indicators_json(indicator_params_path)
+    return i.get("indicators",{}).get(indicator_name)
